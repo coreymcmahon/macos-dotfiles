af052b0b1bd7260469c353dd3debaaf5026e9b34
Corey McMahon <contact@coreymcmahon.com>
2026-01-03 22:12:10 +1100

    Update mac osx defaults

--- macosx-defaults.sh
+++ macosx-defaults.sh
@@ -51,7 +51,7 @@ defaults delete -g NSUserDictionaryReplacementItems
 
 # Disable annoying autocorrections/substitutions
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -int 0
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
@@ -62,11 +62,14 @@ defaults write NSGlobalDomain NSAutomaticEmojiSubstitutionEnabled -bool false
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a fast KeyRepeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1 
-defaults write NSGlobalDomain InitialKeyRepeat -int 4
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
-# Expand save panel by default.
+# Expand save and print panels by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint1 -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
 # Full keyboard access in dialogs
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -102,6 +105,8 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # Always open everything in Finder's list view
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
+defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+
 # Show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
@@ -110,7 +115,6 @@ defaults write com.apple.finder ShowPathbar -bool true
 
 # Disable animations and Get Info animations
 defaults write com.apple.finder DisableAllAnimations -bool true
-
 
 ##
 # Dock
@@ -147,7 +151,7 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Save screenshots to a dedicated folder
 mkdir -p ~/Screenshots
-defaults write com.apple.screencapture location -string "$HOME/Screenshots"
+defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
 
 # Disable screenshot shadows
 defaults write com.apple.screencapture disable-shadow -bool true
@@ -156,10 +160,8 @@ defaults write com.apple.screencapture disable-shadow -bool true
 ##
 # Desktop Services
 
-# Avoid creating .DS_Store files on network volumes.
+# Avoid creating .DS_Store files on network or USB/removable volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# Don't write .DS_Store on USB/removable volumes
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 
@@ -176,7 +178,7 @@ defaults write com.apple.AppleMultitouchTrackpad Clicking 1
 ##
 # Hot corners
 
-# Disable all hot corners
+# Reset all hot corners
 defaults write com.apple.dock wvous-tl-corner -int 0
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
@@ -188,6 +190,10 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 
 defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
+
+# Bottom-left hot corner: Mission Control
+defaults write com.apple.dock wvous-bl-corner -int 2
+defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Top-right hot corner: Lock Screen
 defaults write com.apple.dock wvous-tr-corner -int 13
